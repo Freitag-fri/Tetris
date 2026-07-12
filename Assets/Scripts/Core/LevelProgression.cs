43a032839dd4d8836f926d5781c1ce78fb0683e1
Freitag-fri <sanya007ua@gmail.com>
2026-07-12 11:57:12 +0300

    sorted files into folders

---
+++ Assets/Scripts/Core/LevelProgression.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+
+namespace Assets.Scripts
+{
+    public struct LevelsSettings
+    {
+        public int clearLines;
+        public float stepPeriod;
+        public int level;
+    }
+
+    public static class LevelProgression
+    {
+        static LevelsSettings[] pointsForLinesConfigurationn  = new LevelsSettings[18] {
+            new LevelsSettings { clearLines = 5, stepPeriod = 1.2f, level = 1 },
+            new LevelsSettings { clearLines = 10, stepPeriod = 0.528f, level = 2 },
+            new LevelsSettings { clearLines = 15, stepPeriod = 0.528f, level = 3 },
+            new LevelsSettings { clearLines = 20, stepPeriod = 0.528f, level = 4 },
+            new LevelsSettings { clearLines = 25, stepPeriod = 0.528f, level = 5 },
+            new LevelsSettings { clearLines = 30, stepPeriod = 0.264f, level = 6 },
+            new LevelsSettings { clearLines = 35, stepPeriod = 0.264f, level = 7 },
+            new LevelsSettings { clearLines = 40, stepPeriod = 0.264f, level = 8 },
+            new LevelsSettings { clearLines = 45, stepPeriod = 0.264f, level = 9 },
+            new LevelsSettings { clearLines = 50, stepPeriod = 0.132f, level = 10 },
+            new LevelsSettings { clearLines = 55, stepPeriod = 0.066f, level = 11 },
+            new LevelsSettings { clearLines = 60, stepPeriod = 0.066f, level = 12 },
+            new LevelsSettings { clearLines = 65, stepPeriod = 0.066f, level = 13 },
+            new LevelsSettings { clearLines = 70, stepPeriod = 0.033f, level = 14 },
+            new LevelsSettings { clearLines = 75, stepPeriod = 0.033f, level = 15 },
+            new LevelsSettings { clearLines = 80, stepPeriod = 0.016f, level = 16 },
+            new LevelsSettings { clearLines = 85, stepPeriod = 0.016f, level = 17 },
+            new LevelsSettings { clearLines = 90, stepPeriod = 0.016f, level = 18 }
+        };
+
+        public static LevelsSettings GetLevelSettingsByClearLines(int totalNumberClearLines)
+        {
+            return pointsForLinesConfigurationn.FirstOrDefault(v => v.clearLines > totalNumberClearLines);
+        }
+    }
+}
