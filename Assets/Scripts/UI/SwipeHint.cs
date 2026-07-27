da4dfd523db42a9df90094296cd41882d59832bc
Freitag-fri <sanya007ua@gmail.com>
2026-07-28 01:39:45 +0300

    add parametr to animator

--- Assets/Scripts/UI/SwipeHint.cs
+++ Assets/Scripts/UI/SwipeHint.cs
@@ -6,33 +6,34 @@ public class SwipeHint : MonoBehaviour
 {
     [SerializeField] private int maxShows = 3;
     [SerializeField] private float delay = 2.5f;
+    [SerializeField] private Animator animator;
     private Tween tween;
 
-    // Show after delay, but only the first few times
+    // Show after delay, but only the first few times (maxShows)
     public void Show()
     {
-        tween?.Kill();
-        gameObject.SetActive(false);
+        Hide();
 
         if (SaveData.SwipeHintShowCount >= maxShows)
             return;
 
-        tween = DOVirtual.DelayedCall(delay, () => gameObject.SetActive(true));
+        tween = DOVirtual.DelayedCall(delay, () => {
+            gameObject.SetActive(true);
+            animator.SetBool("bStartAnimation", true);
+        });
     }
 
     public void HideAfterSwipe()
     {
-        tween?.Kill();
-
         if (gameObject.activeSelf)
             SaveData.SwipeHintShowCount++;
-
-        gameObject.SetActive(false);
+        Hide();
     }
 
     public void Hide()
     {
         tween?.Kill();
+        animator.SetBool("bStartAnimation", false);
         gameObject.SetActive(false);
     }
 }
